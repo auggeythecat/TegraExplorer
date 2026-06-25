184635e277bc8d40809e0ecbf240e919d78ed3d0
auggeythecat <noahthomassymonds@gmail.com>
2026-06-25 19:53:16 -0400

    Improve hid to be more functional and usable.

--- source/util/hid.c
+++ source/util/hid.c
@@ -5,65 +5,55 @@
 #include "hid.h"
 #include "config.h"
 
-static input_t inputs = {0};
+input_t inputs = {0};
 u16 LbaseX = 0, LbaseY = 0, RbaseX = 0, RbaseY = 0;
 
 void hidInit(){
     jc_init_hw();
+    joycon_poll();
 }
 
 input_t *hidRead(){
     jc_gamepad_rpt_t *controller = joycon_poll();
 
     inputs.buttons = 0;
-    u8 left_connected = 0;
-    u8 right_connected = 0;
 
     if (controller != NULL){
-        if (controller->home && !TEConfig.t210b01)
-            // RebootToPayloadOrRcm();
-
-        if (controller->cap)
-            // TakeScreenshot();
-
         inputs.buttons = controller->buttons;
 
-        left_connected = controller->conn_l;
-        right_connected = controller->conn_r;
-    }
+        if (controller->conn_l) {
+            if ((LbaseX == 0 || LbaseY == 0) || controller->l3){
+                LbaseX = controller->lstick_x;
+                LbaseY = controller->lstick_y;
+            }
 
+            inputs.up    = (controller->up    || inputs.volp || (controller->lstick_y > LbaseY + 500)) ? 1 : 0;
+            inputs.down  = (controller->down  || inputs.volm || (controller->lstick_y < LbaseY - 500)) ? 1 : 0;
+            inputs.left  = (controller->left  || (controller->lstick_x < LbaseX - 500))                ? 1 : 0;
+            inputs.right = (controller->right || (controller->lstick_x > LbaseX + 500))                ? 1 : 0;
+        } else {
+            inputs.up    = inputs.volp;
+            inputs.down  = inputs.volm;
+        }
+
+        if (controller->conn_r) {
+            if ((RbaseX == 0 || RbaseY == 0) || controller->r3){
+                RbaseX = controller->rstick_x;
+                RbaseY = controller->rstick_y;
+            }
+
+            inputs.rUp    = (controller->rstick_y > RbaseY + 500) ? 1 : 0;
+            inputs.rDown  = (controller->rstick_y < RbaseY - 500) ? 1 : 0;
+            inputs.rLeft  = (controller->rstick_x < RbaseX - 500) ? 1 : 0;
+            inputs.rRight = (controller->rstick_x > RbaseX + 500) ? 1 : 0;
+        }
+    }
 
     u8 btn = btn_read();
-    inputs.volp = (btn & BTN_VOL_UP) ? 1 : 0;
-    inputs.volm = (btn & BTN_VOL_DOWN) ? 1 : 0;
-    inputs.power = (btn & BTN_POWER) ? 1 : 0;
+    inputs.volp  = (btn & BTN_VOL_UP)   ? 1 : 0;
+    inputs.volm  = (btn & BTN_VOL_DOWN) ? 1 : 0;
+    inputs.power = (btn & BTN_POWER)    ? 1 : 0;
 
-    if (left_connected) {
-        if ((LbaseX == 0 || LbaseY == 0) || controller->l3){
-            LbaseX = controller->lstick_x;
-            LbaseY = controller->lstick_y;
-        }
-
-        inputs.up = (controller->up || inputs.volp || (controller->lstick_y > LbaseY + 500)) ? 1 : 0;
-        inputs.down = (controller->down || inputs.volm || (controller->lstick_y < LbaseY - 500)) ? 1 : 0;
-        inputs.left = (controller->left || (controller->lstick_x < LbaseX - 500)) ? 1 : 0;
-        inputs.right = (controller->right || (controller->lstick_x > LbaseX + 500)) ? 1 : 0;
-    } else {
-        inputs.up = inputs.volp;
-        inputs.down = inputs.volm;
-    }
-
-    if (right_connected) {
-        if ((RbaseX == 0 || RbaseY == 0) || controller->r3){
-            RbaseX = controller->rstick_x;
-            RbaseY = controller->rstick_y;
-        }
-
-        inputs.rUp = (controller->rstick_y > RbaseY + 500) ? 1 : 0;
-        inputs.rDown = (controller->rstick_y < RbaseY - 500) ? 1 : 0;
-        inputs.rLeft = (controller->rstick_x < RbaseX - 500) ? 1 : 0;
-        inputs.rRight = (controller->rstick_x > RbaseX + 500) ? 1 : 0;
-    }
     inputs.a = inputs.a || inputs.power;
 
     return &inputs;
--- source/util/hid.h
+++ source/util/hid.h
@@ -7,24 +7,42 @@
 
 #include "bdk.h"
 
-#define JoyY BIT(0)
-#define JoyX BIT(1)
-#define JoyB BIT(2)
-#define JoyA BIT(3)
-#define JoyRB BIT(6)
-#define JoyMenu BIT(12)
-#define JoyLDown BIT(16)
-#define JoyLUp BIT(17)
+#define JoyY      BIT(0)
+#define JoyX      BIT(1)
+#define JoyB      BIT(2)
+#define JoyA      BIT(3)
+#define JoySR_R   BIT(4)
+#define JoySL_R   BIT(5)
+#define JoyR      BIT(6)
+#define JoyZR     BIT(7)
+
+#define JoyMINUS  BIT(8)
+#define JoyPLUS   BIT(9)
+#define JoyR3     BIT(10)
+#define JoyL3     BIT(11)
+#define JoyHOME   BIT(12)
+#define JoyCAP    BIT(13)
+#define JoyPAD    BIT(14)
+#define JoyWIRED  BIT(15)
+
+#define JoyLDown  BIT(16)
+#define JoyLUp    BIT(17)
 #define JoyLRight BIT(18)
-#define JoyLLeft BIT(19)
-#define JoyLB BIT(22)
-#define BtnPow BIT(24)
-#define BtnVolP BIT(25)
-#define BtnVolM BIT(26)
-#define JoyRDown BIT(27)
-#define JoyRUp BIT(28)
+#define JoyLLeft  BIT(19)
+#define JoySR_L   BIT(20)
+#define JoySL_L   BIT(21)
+#define JoyL      BIT(22)
+#define JoyZL     BIT(23)
+
+#define BtnPow    BIT(24)
+#define BtnVolP   BIT(25)
+#define BtnVolM   BIT(26)
+
+#define JoyRDown  BIT(27)
+#define JoyRUp    BIT(28)
 #define JoyRRight BIT(29)
-#define JoyRLeft BIT(30)
+#define JoyRLeft  BIT(30)
+#define JOYUNUSED BIT(31)
 
 #define WAITBUTTONS (JoyY | JoyX | JoyB | JoyA | JoyLDown | JoyLUp | JoyLRight | JoyLLeft)
 
@@ -32,52 +50,54 @@ typedef struct _input_t {
     union {
         struct {
             // Joy-Con (R).
-            u32 y:1;
-            u32 x:1;
-            u32 b:1;
-            u32 a:1;
-            u32 sr_r:1;
-            u32 sl_r:1;
-            u32 r:1;
-            u32 zr:1;
+            u32 y     :1;
+            u32 x     :1;
+            u32 b     :1;
+            u32 a     :1;
+            u32 sr_r  :1;
+            u32 sl_r  :1;
+            u32 r     :1;
+            u32 zr    :1;
 
             // Shared
-            u32 minus:1;
-            u32 plus:1;
-            u32 r3:1;
-            u32 l3:1;
-            u32 home:1;
-            u32 cap:1;
-            u32 pad:1;
-            u32 wired:1;
+            u32 minus :1;
+            u32 plus  :1;
+            u32 r3    :1;
+            u32 l3    :1;
+            u32 home  :1;
+            u32 cap   :1;
+            u32 pad   :1;
+            u32 wired :1;
 
             // Joy-Con (L).
-            u32 down:1;
-            u32 up:1;
-            u32 right:1;
-            u32 left:1;
-            u32 sr_l:1;
-            u32 sl_l:1;
-            u32 l:1;
-            u32 zl:1;
+            u32 down  :1;
+            u32 up    :1;
+            u32 right :1;
+            u32 left  :1;
+            u32 sr_l  :1;
+            u32 sl_l  :1;
+            u32 l     :1;
+            u32 zl    :1;
 
-            u32 power:1;
-            u32 volp:1;
-            u32 volm:1;
+            u32 power :1;
+            u32 volp  :1;
+            u32 volm  :1;
 
-            u32 rDown:1;
-            u32 rUp:1;
+            u32 rDown :1;
+            u32 rUp   :1;
             u32 rRight:1;
-            u32 rLeft:1;
+            u32 rLeft :1;
+
+            u32 unused:1;
         };
         u32 buttons;
     };
 } input_t;
 
-void hidInit();
+void     hidInit();
 input_t *hidRead();
 input_t *hidWait();
 input_t *hidWaitMask(u32 mask);
-bool hidConnected();
+bool     hidConnected();
 
 #endif //_HID_H
