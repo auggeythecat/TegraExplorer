a0b88e8576d21761c3e845c9a50662d3cd65e340
auggeythecat <noahthomassymonds@gmail.com>
2026-08-09 01:34:19 -0400

    Clean main test menu.

--- source/menus/mainMenu.c
+++ source/menus/mainMenu.c
@@ -2,51 +2,35 @@
 // Created by auggiethecat on 5/3/26.
 //
 
+#include "mainMenu.h"
+
 #include <bdk.h>
 
 #include "../gfx/menu.h"
 #include "../gfx/gfx.h"
 #include "../util/utils.h"
 
-u32 getCount(void* stuff) {
-    return vecGetCount(stuff);
-}
 
-menu_t SDMenu = {
-    .title = "Browse SD",
-    .x     = 0,    .y = 0,
-    .w     = SCREEN_WIDTH,
-    .h     = SCREEN_HEIGHT,
-    .idx   = 0,    .offset = 0,
+#include "explorers.h"
 
-    .isOverlay     = false,
-    .isDynamic     = true ,
-    .isPageCount   = true ,
-    .isFileBrowser = true ,
-    .cursorIndex   = 3    ,
-
-    .__dynamic.data        = "sd:/",
-    .__dynamic.get_count   = getCount,
-    .__dynamic.get_entries = NULL,
-};
 entry_t mainMenuEntries[] = {
-    ENT_SEPERATOR(                                                                     ),
-      ENT_CAPTION(COLOR_WHITE ,  "---| Browse |---"                                    ),
-         ENT_MENU(COLOR_GREEN , "Browse SD",                               &SDMenu     ),
-      ENT_HANDLER(COLOR_GREEN , "Browse SysMMC",                           rebootRCM   ),
-      ENT_HANDLER(COLOR_BLUE  , "Browse EmuMMC",                           reboot      ),
+     ENT_SEPERATOR(                                                                       ),
+       ENT_CAPTION(COLOR_WHITE ,  "---| Browse |---"                                      ),
+    ENT_HANDLER_EX(COLOR_GREEN , "Browse SD",                        "sd:", pushFolderMenu),
+       ENT_HANDLER(COLOR_GREEN , "Browse SysMMC",                           rebootRCM     ),
+       ENT_HANDLER(COLOR_BLUE  , "Browse EmuMMC",                           reboot        ),
 
-      ENT_CAPTION(COLOR_WHITE ,  "---| Exit |---"                                      ),
-      ENT_HANDLER(COLOR_VIOLET, "Power off",                               powerOff    ),
-      ENT_HANDLER(COLOR_VIOLET, "Reboot to RCM",                           rebootRCM   ),
-      ENT_HANDLER(COLOR_VIOLET, "Reboot to OFW",                           reboot      ),
-      ENT_HANDLER(COLOR_VIOLET, "Reboot to bootloader/update.bin",         rebootHekate),
-      ENT_HANDLER(COLOR_VIOLET, "Reboot to atmosphere/reboot_payload.bin", rebootAMS   ),
+       ENT_CAPTION(COLOR_WHITE ,  "---| Exit |---"                                        ),
+       ENT_HANDLER(COLOR_VIOLET, "Power off",                               powerOff      ),
+       ENT_HANDLER(COLOR_VIOLET, "Reboot to RCM",                           rebootRCM     ),
+       ENT_HANDLER(COLOR_VIOLET, "Reboot to OFW",                           reboot        ),
+       ENT_HANDLER(COLOR_VIOLET, "Reboot to bootloader/update.bin",         rebootHekate  ),
+       ENT_HANDLER(COLOR_VIOLET, "Reboot to atmosphere/reboot_payload.bin", rebootAMS     ),
     // MainTools,
     // MainPartitionSd,
     // MainViewKeys,
     // MainViewCredits,
-    ENT_END()
+     ENT_END()
 
 };
 
@@ -61,7 +45,7 @@ menu_t mainMenu = {
     .isDynamic     = false,
     .isPageCount   = true ,
     .isFileBrowser = false,
-    .cursorIndex   = 3,
+    .cursorIndex   = 2,
 
     .__static.entries = mainMenuEntries,
     .__static.count   = ARRAY_SIZE(mainMenuEntries)
