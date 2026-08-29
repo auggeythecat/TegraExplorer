de83362b3b58b02e82cd354e0c6800cd0562558a
auggeythecat <noahthomassymonds@gmail.com>
2026-08-28 23:20:53 -0400

    source: utils: preliminary/basic utils.

---
+++ source/util/utils.h
@@ -0,0 +1,27 @@
+/*—————————————————————————————————————————————————————————————————————————————
+ — Copyright (c) 2026 auggiethecat!                                           —
+ —                                                                            —
+ — All code was handwritten without the assistance of a Large Language Model. —
+ —                                                                            —
+ — This program is free software; you can redistribute it and/or modify it    —
+ — under the terms and conditions of the GNU General Public License,          —
+ — version 2, as published by the Free Software Foundation.                   —
+ —                                                                            —
+ — This program is distributed in the hope it will be useful, but WITHOUT     —
+ — ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or      —
+ — FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for  —
+ — more details.                                                              —
+ —                                                                            —
+ — You should have received a copy of the GNU General Public License          —
+ — along with this program.  If not, see <http://www.gnu.org/licenses/>.      —
+ —————————————————————————————————————————————————————————————————————————————*/
+
+#ifndef _UTILS_H
+#define _UTILS_H
+
+void powerOff();
+void rebootRCM();
+void rebootFuseBypass();
+void rebootOFW();
+
+#endif //_UTILS_H
