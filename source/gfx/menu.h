6b99dfb80b1a40ca0cf717eaf180ff36403fadb8
auggeythecat <auggiethecat@gmail.com>
2026-09-02 22:46:35 -0500

    bugfixes: source: menu:
    Bug #3: Make only file and directory entries selectable.
    
    I seriously don't understand why I did anything else.

--- source/gfx/menu.h
+++ source/gfx/menu.h
@@ -70,11 +70,11 @@ typedef struct _menuEntry_t {
 #define  ENT_SEPERATOR(                             ) { ENTRY_SEPERATOR , 0      , COLOR_NONE, NULL   , NULL, { .renderDirty = 1, .skip = 1, .hide = 1           } }
 #define    ENT_CAPTION(color, caption               ) { ENTRY_CAPTION   , caption, color     , NULL   , NULL, { .renderDirty = 1, .skip = 1                      } }
 #define    ENT_HANDLER(color, caption,       handler) { ENTRY_HANDLER   , caption, color     , handler, NULL, { .renderDirty = 1,                                } }
-#define ENT_HANDLER_EX(color, caption, data, handler) { ENTRY_HANDLER_EX, caption, color     , handler, data, { .renderDirty = 1, .selectable = 1                } }
-#define       ENT_MENU(color, caption, data, handler) { ENTRY_MENU      , caption, color     , handler, data, { .renderDirty = 1, .selectable = 1                } }
+#define ENT_HANDLER_EX(color, caption, data, handler) { ENTRY_HANDLER_EX, caption, color     , handler, data, { .renderDirty = 1,                                } }
+#define       ENT_MENU(color, caption, data, handler) { ENTRY_MENU      , caption, color     , handler, data, { .renderDirty = 1,                                } }
 #define  ENT_DIRECTORY(color, caption, data, handler) { ENTRY_DIRECTORY , caption, color     , NULL   , data, { .renderDirty = 1, .selectable = 1, .showSize = 1 } }
 #define       ENT_FILE(color, caption, data, handler) { ENTRY_FILE      , caption, color     , NULL   , data, { .renderDirty = 1, .selectable = 1  .showSize = 1 } }
-#define       ENT_BACK(color, caption               ) { ENTRY_BACK      , caption, color     , NULL   , NULL, { .renderDirty = 1, .selectable = 1                } }
+#define       ENT_BACK(color, caption               ) { ENTRY_BACK      , caption, color     , NULL   , NULL, { .renderDirty = 1,                                } }
 
 typedef struct _menu_t {
     const char* title;
