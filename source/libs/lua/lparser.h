7503d8cc0fde94372d10508656fb5fbf4fdac3ed
auggeythecat <noahthomassymonds@gmail.com>
2026-09-24 11:28:27 -0500

    source: lua: strip out runtime compilation, (plus simple porting notes).

--- source/libs/lua/lparser.h
+++ source/libs/lua/lparser.h
@@ -7,52 +7,10 @@
 #ifndef lparser_h
 #define lparser_h
 
-#include "llimits.h"
 #include "lobject.h"
 #include "lzio.h"
 
-
-/*
-** Expression descriptor
-*/
-
-typedef enum {
-  VVOID,	/* no value */
-  VNIL,
-  VTRUE,
-  VFALSE,
-  VK,		/* info = index of constant in `k' */
-  VKNUM,	/* nval = numerical value */
-  VNONRELOC,	/* info = result register */
-  VLOCAL,	/* info = local register */
-  VUPVAL,       /* info = index of upvalue in 'upvalues' */
-  VINDEXED,	/* t = table register/upvalue; idx = index R/K */
-  VJMP,		/* info = instruction pc */
-  VRELOCABLE,	/* info = instruction pc */
-  VCALL,	/* info = instruction pc */
-  VVARARG	/* info = instruction pc */
-} expkind;
-
-
-#define vkisvar(k)	(VLOCAL <= (k) && (k) <= VINDEXED)
-#define vkisinreg(k)	((k) == VNONRELOC || (k) == VLOCAL)
-
-typedef struct expdesc {
-  expkind k;
-  union {
-    struct {  /* for indexed variables (VINDEXED) */
-      short idx;  /* index (R/K) */
-      lu_byte t;  /* table (register or upvalue) */
-      lu_byte vt;  /* whether 't' is register (VLOCAL) or upvalue (VUPVAL) */
-    } ind;
-    int info;  /* for generic use */
-    lua_Number nval;  /* for VKNUM */
-  } u;
-  int t;  /* patch list of `exit when true' */
-  int f;  /* patch list of `exit when false' */
-} expdesc;
-
-
+/* Preserved for Dyndata and it's use in ldo */
 /* description of active local variable */
 typedef struct Vardesc {
   short idx;  /* variable index in stack */
@@ -87,33 +45,19 @@ typedef struct Dyndata {
   Labellist label;   /* list of active labels */
 } Dyndata;
 
-
-/* control of blocks */
-struct BlockCnt;  /* defined in lparser.c */
-
-
-/* state needed to generate code for a given function */
-typedef struct FuncState {
-  Proto *f;  /* current function header */
-  Table *h;  /* table to find (and reuse) elements in `k' */
-  struct FuncState *prev;  /* enclosing function */
-  struct LexState *ls;  /* lexical state */
-  struct BlockCnt *bl;  /* chain of current blocks */
-  int pc;  /* next position to code (equivalent to `ncode') */
-  int lasttarget;   /* 'label' of last 'jump label' */
-  int jpc;  /* list of pending jumps to `pc' */
-  int nk;  /* number of elements in `k' */
-  int np;  /* number of elements in `p' */
-  int firstlocal;  /* index of first local var (in Dyndata array) */
-  short nlocvars;  /* number of elements in 'f->locvars' */
-  lu_byte nactvar;  /* number of active local variables */
-  lu_byte nups;  /* number of upvalues */
-  lu_byte freereg;  /* first free register */
-} FuncState;
-
-
 LUAI_FUNC Closure *luaY_parser (lua_State *L, ZIO *z, Mbuffer *buff,
-                                Dyndata *dyd, const char *name, int firstchar);
+                                void *dyd, const char *name, int firstchar)
+{
+  UNUSED(buff);
+  UNUSED(z);
+  UNUSED(dyd);
+  UNUSED(name);
+  UNUSED(firstchar);
+
+  lua_error(L);
+
+  return NULL;
+}
 
 
 #endif
